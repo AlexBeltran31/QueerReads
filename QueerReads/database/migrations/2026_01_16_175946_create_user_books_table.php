4700dd513fec2a495e1617508353a453dc9ed604
AlexBeltran31 <alexbeltranrt@gmail.com>
2026-01-16 19:12:28 +0100

    Migrations done

---
+++ QueerReads/database/migrations/2026_01_16_175946_create_user_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
+            $table->enum('status', ['to_read', 'reading', 'finished'])->default('to_read');
+            $table->unsignedTinyInteger('rating')->nullable;
+            $table->check('rating >= 1 AND rating <=5');
+            $table->text('review')->nullable();
+            $table->timestamps();
+
+            $table->unique(['user_id', 'book_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_books');
+    }
+};
